daa57587ead6c96e12ff5370b21775a1f6de8675
Justin Daniel <justinddaniel@gmail.com>
2017-11-15 01:52:12 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,5 +6,22 @@ class Song < ActiveRecord::Base
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
   end
-  
+
+  def artist_name
+    self.artist.name
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre.name
+  end
+
+  def note_contents=(contents)
+    if !!contents
+      self.notes << Notes.find_by(contents: contents) if Notes.find_by(contents: contents)
+    end
+  end
 end
